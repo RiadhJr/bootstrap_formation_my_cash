89f3bb889f3ee40ea9bf57adc182d080ab044c02
riadh_jarray <riadh.j@db-consulting-group.com>
2022-01-31 17:16:46 +0100

    mise a jour

--- js/main.js
+++ js/main.js
@@ -44,6 +44,63 @@ $(document).ready(function() {
       }
   });
 
+  /* ---------------------- screenshots carousel ---------------- */
+  $('.screenshots-carousel').owlCarousel({
+    loop:true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:4,
+        }
+    }
+});
+
+/* ---------------------- testimonials carousel ---------------- */
+$('.testimonials-carousel').owlCarousel({
+  loop:true,
+  margin: 0,
+  autoplay: true,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+      },
+      600:{
+          items:2,
+      },
+      1000:{
+          items:3,
+      }
+  }
+});
+
+/* ---------------------- team carousel ---------------- */
+$('.team-carousel').owlCarousel({
+  loop:true,
+  margin: 0,
+  autoplay: true,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+      },
+      600:{
+          items:2,
+      },
+      1000:{
+          items:3,
+      }
+  }
+});
+
 
 
 });
